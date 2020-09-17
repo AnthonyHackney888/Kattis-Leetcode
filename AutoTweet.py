c048d72f47412e341db926f58c09580334272c77
Anthony Hackney <60517860+AnthonyHackney888@users.noreply.github.com>
2020-09-17 17:05:51 -0400

    Update AutoTweet.py
    changed the auto tweeter so that it would work.

--- AutoTweet.py
+++ AutoTweet.py
@@ -1,51 +1,42 @@
 import tweepy
 import time
-from tkinter import filedialog
-from tkinter import *
-
 
 auto_message_greet = 'AUTO TWEET: '
-target_time = "19:06:00"
+
+target_time = "16:54:00"
+message = 'Step 1: create auto tweeter that works.'
+
+auth = tweepy.OAuthHandler()
+auth.set_access_token()
+
 
 def time_set():
   return time.strftime("%H:%M:%S")
 
+def check_credentials():
+    api = tweepy.API(auth)
+    if api.verify_credentials() == False:
+        print('auth failed')
+    else:
+        print('auth ok')
 
-def open_dialog():
-  auth = tweepy.OAuthHandler()
-  auth.set_access_token()
+def tweet_func():
   api = tweepy.API(auth)
-  r.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("txt files","*.txt"),("all files","*.*")))
-  with open(r.filename, 'r') as f:
-    while True:
-      while time_set() != target_time:
-        time.sleep(.01)
-      if time_set() == target_time:
-        print('got here 2')
+  check_credentials()
+  while time_set != target_time:
+    time.sleep(.001)
+    if time_set() == target_time:
         try:
-          api.verify_credentials()
-          print('auth OK')
-        except:
-          print('error sorry B')
-      try:
-        test1 = f.readline()
-        print(test1)
-        if f != '\n':
-          print('a')
-          api.update_status(auto_message_greet + test1)
-          break
-      except tweepy.TweepError as e:
-          print(e.reason)
-    #api.update_status(line)
-    #api.update_status("test")
+            api.update_status(auto_message_greet + message)
+        except tweepy.TweepError as e:
+            print(e.reason)      
+tweet_func()
+    
+#api.update_status(line)
+#api.update_status("test")
 
 
 
-r = Tk()
-r.title('choose text file')
-button = Button(r, text='open', width=25,command=open_dialog)
-button.pack()
-r.mainloop()
 
 
 
