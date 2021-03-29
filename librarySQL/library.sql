52f6bd4f0249003b76496be74c665ef5297d17e9
Anthony Hackney <60517860+AnthonyHackney888@users.noreply.github.com>
2021-03-28 21:13:33 -0400

    Update library.sql
    updated SQL to match UML

--- librarySQL/library.sql
+++ librarySQL/library.sql
@@ -12,3 +12,30 @@ drop table if exists userDb;
  insert into userDb values(1, 'Anthony', 'Hackney', 5,0);
  insert into userDb values(2, 'Viola', 'Svern', 2,1);
  select * from userDb;
+ 
+ drop table if exists libEmployee;
+ 
+ create table libEmployee(
+	empID int, 
+	fname varchar(50),
+	lname varchar(50), 
+	HoursLogged double,
+	HoursScheduled double
+	primary key(empID));
+	
+insert into libEmployee values(1, 'Anthony','notHackney',20.5,33.0);
+select * from libEmployee;
+
+drop table if exists libManager;
+ 
+ create table libManager(
+	managerID int, 
+	fname varchar(50),
+	lname varchar(50), 
+	HoursLogged double,
+	HoursScheduled double
+	primary key(managerID));
+	
+
+insert into libManager values(1,'Manager', 'Hackney',15,45);
+select * from libManager;
