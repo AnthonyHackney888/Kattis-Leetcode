9fa6a519ab20f0970d08a38a53d89bec4ea53e99
Anthony Hackney <60517860+AnthonyHackney888@users.noreply.github.com>
2021-04-07 14:33:59 -0400

    Add files via upload
    Edited the UML for project and created shell for the classes

---
+++ librarySQL/employee.java
@@ -0,0 +1,18 @@
+public class employee {
+    public boolean addUser(){
+        return false;
+
+    }
+
+    public boolean removeUser(){
+        return false;
+
+    }
+    public boolean returns(){
+        return false;
+
+    }
+    public void checkOut(){
+
+    }
+}
---
+++ librarySQL/user.java
@@ -0,0 +1,14 @@
+public class user{
+
+    public final String fName;
+    public final String lName;
+    public final int booksCheckedOut;
+    public final int userID;
+
+
+    public static int checkOut(){
+        return 0;
+    }
+    
+
+}
