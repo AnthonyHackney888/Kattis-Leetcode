68ca8ba829c37fb5576932a198218f20fda79c86
Anthony Hackney <60517860+AnthonyHackney888@users.noreply.github.com>
2021-04-14 15:15:21 -0400

    Added Constructors
    As well added the basis for updating user information from the employee class.

--- librarySQL/clerk.java
+++ librarySQL/clerk.java
@@ -1,25 +1,34 @@
 public class clerk extends employee {
-    @Override
-    public boolean addUser(){
-        return false;
+
+    private int empId;
+    public clerk(String fName, String lName, int empId){
+        super(fName, lName);
+        this.empId = empId;
+    }
+
+    
+
+    /**
+     * @return int return the empId
+     */
+    public int getEmpId() {
+        return empId;
+    }
+
+    /**
+     * @param empId the empId to set
+     */
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+
+  
+    public void addUser(int id){
 
     }
-    @Override
-    public boolean removeUser(){
-        return false;
+    
+    public void removeUser(int id){
 
-    }
-    @Override
-    public boolean returns(){
-        return false;
-
-    }
-    @Override
-    public void checkOut(){
-        
-    }
-    public int emplyeeID(){
-        return 0;
-        
     }
 }
